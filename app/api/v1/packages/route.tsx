96690185942d6489b1ff1578798ed9ad03ef441d
andresin87 <andresin87@gmail.com>
2023-06-28 23:18:28 +0200

    remove api route directory

--- app/api/v1/packages/route.tsx
+++
@@ -1,9 +0,0 @@
-import { NextResponse, NextRequest } from "next/server";
-
-export async function GET(request: Request, context: { params: any }) {
-  return NextResponse.json({
-    response: {
-
-    },
-  });
-}
